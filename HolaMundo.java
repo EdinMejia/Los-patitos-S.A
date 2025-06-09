6f1936c5725539ac81b6d375b8e3069bbe3ae804
Edin Mejía <edinmejia1208@gmail.com>
2025-06-09 16:23:43 -0600

    update 1

---
+++ HolaMundo.java
@@ -0,0 +1,16 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprime un saludo
+        System.out.println("Hola, mundo!");
+
+        // Declara dos números
+        int numero1 = 5;
+        int numero2 = 7;
+
+        // Suma los números
+        int suma = numero1 + numero2;
+
+        // Muestra el resultado
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es " + suma);
+    }
+}
